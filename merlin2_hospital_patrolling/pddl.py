69fe937ff25e7edbb1cce90dbaee2898840fbeea
TheSmileXiao <yx879894213@gmail.com>
2024-06-10 00:11:32 +0200

    corregir los errores

--- merlin2_hospital_patrolling/pddl.py
+++ merlin2_hospital_patrolling/pddl.py
@@ -1,1 +1,10 @@
 # TODO: write the PDDL domain
+
+from merlin2_basic_actions.merlin2_basic_types import wp_type
+from kant_dto import PddlTypeDto, PddlPredicateDto
+
+# types
+room_type = PddlTypeDto("room")
+# pre
+room_patrolled = PddlPredicateDto("room_patrolled", [room_type])
+room_at = PddlPredicateDto("room_at", [room_typem wp_type])
